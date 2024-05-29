1c910fcbe5d5b1d56f1a8f59cc51f84a4750183f
ligalah <ligalahh@gmail.com>
2024-05-29 09:18:37 +0300

    Layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import { ClerkProvider, SignInButton, SignIn, SignedOut, UserButton} from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import '../globals.css';
+
+export const metadata = {
+    title: 'Community',
+    description: 'A social application'
+}
+
+const inter = Inter({subsets:["latin"]});
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}){
+    return (
+    <ClerkProvider>
+        <html lang="en">
+            <body className={`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+        </html>
+
+    </ClerkProvider>
+)
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Community",
+  description: "A social Application",
+};
+
+export default function RootLayout({
+  children,
+}:{
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          
+          <main className="flex flex-row">
+            <LeftSidebar />
+
+              <section className="main-container">
+                <div className="w-full max-w-4xl">
+                  {children}
+                </div>
+              </section>
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+    
+  );
+}
